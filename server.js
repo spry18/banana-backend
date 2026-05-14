d8e5d0767b03aec2076baae966aa0f29554b276c
supriya <spryshnde@gmail.com>
2026-05-14 22:02:09 +0530

    Testing backend auto deploy

--- server.js
+++ server.js
@@ -130,4 +130,5 @@ const PORT = process.env.PORT || 5000;
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log("Backend Auto Deploy Working");
 });
