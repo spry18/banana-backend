55ea59324db01ddab0b75e88e9199b00aa645ed8
supriya <spryshnde@gmail.com>
2026-06-20 12:04:49 +0530

    fix: ncreasing the max rate limiter value allows more requests within the defined windowMs. CoreUI

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(helmet({
 // Rate Limiter
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 200, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again after 15 minutes'
 });
 app.use('/api', limiter);
